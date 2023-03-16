71adb794e49ed6b988d30e695fff762d91b1daa1
Javier Suarez <cusitosr88@gmail.com>
2023-03-16 18:32:22 -0400

    add nuxt loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="min-h-screen bg-slate-800">
+        <NuxtLoadingIndicator />
         <Header/>
         <main class="mx-auto px-0 lg:px-0 pt-14 lg:pt-12 h-3/4">
             <slot />
