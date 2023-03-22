7842b833c784e4ea4e1257d85f4312b67decef34
Javier Suarez <cusitosr88@gmail.com>
2023-03-22 19:55:18 -0400

    set loading indicator blue color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen bg-slate-800">
-        <NuxtLoadingIndicator />
+        <NuxtLoadingIndicator color="#0ea5e9" />
         <Header/>
         <main class="mx-auto px-0 lg:px-0 pt-14 lg:pt-12 h-3/4">
             <slot />
